e8ec74c52ef2242bf580332585b32fc7f9efceaf
taxfraud49 <abelikovs18@gmail.com>
2025-04-26 12:37:33 +0300

    dvh

--- app/(client)/signin/page.jsx
+++ app/(client)/signin/page.jsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 
 export default function SignIn() {
     return (
-        <section className="h-screen w-full flex flex-col justify-center items-center">
+        <section className="h-[100dvh] w-full flex flex-col justify-center items-center">
             <div>
-            <h1 className="mb-5 heading">Gym<span className="blue-span">Lense</span></h1>
-            <div className="w-full">
-                <button className="sign-in-button flex justify-center gap-2 items-center"><Image src="/apple-logo.svg" height={19} width={19} alt="" />Sign in with Apple</button>
-                <button className="sign-in-button flex justify-center gap-2 items-center"><Image src="/google-logo.svg" height={19} width={19} alt="" />Sign in with Google</button>
-            </div>
+                <h1 className="mb-5 heading">Gym<span className="blue-span">Lense</span></h1>
+                <div className="w-full">
+                    <button className="sign-in-button flex justify-center gap-2 items-center"><Image src="/apple-logo.svg" height={19} width={19} alt="" />Sign in with Apple</button>
+                    <button className="sign-in-button flex justify-center gap-2 items-center"><Image src="/google-logo.svg" height={19} width={19} alt="" />Sign in with Google</button>
+                </div>
             </div>
         </section>
     )
